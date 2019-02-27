09782ae654985f4dca3c8d2bfb1f0eba11c3e97b
katlynHedgpeth <katlyn.hedgpeth@gmail.com>
2019-02-27 13:47:45 -0600

    Target presentation, OG task start for maintain

--- experiment/monitain_v1.py
+++ experiment/monitain_v1.py
@@ -9,19 +9,84 @@
 
 import random
 import numpy as np 
-import psychopy.visual
-import psychopy.event
+from psychopy import visual, event, core
 
-win = psychopy.visual.Window(
-	size=[400,400],
+color_white = [1,1,1]
+color_black = [-1,-1,-1]
+color_gray = [0,0,0]
+
+win = visual.Window(
+	size=[1024,576], #Small size of screen for testing it out
 	units="pix", 
 	fullscr=False, #set to True when running for real
-	color=[1,1,1])
+	color=color_white)
+
+## MAINTAIN ## 
+
+###Target
+
+grating = visual.GratingStim(
+	win=win,
+	units="pix", 
+	size=[150,150], #size of box with grating in pixels
+	mask = "circle"
+	) 
+
+grating.pos = [0.0,0.0]
+grating.ori = 45.0
+grating.sf = 5.0 / 80.0 #spatialFrequency = # of cycles / length in pixels 
+grating.contrast = 1.0
+
+grating.draw()
+
+win.flip()
+
+sec = 3
+core.wait(sec)
+
+win.color=color_gray
+win.flip()
+
+###OG task
+
+# Start a loop for this
+
+text = visual.TextStim(
+	win=win, 
+	text="word", 
+	color=color_black, 
+	height = 40.0)
+text.draw()
+
+
+win.flip()
+
+sec = 2 
+core.wait(sec)
+
+# End loop
+
+###Target probe
+
+
+win.close() #Close window
+
+
+
+
+
+
+
+
+
+
+
+
 
 text = psychopy.visual.TextStim(
 	win=win, 
 	text="Welcome to the experiment!", 
-	color=[-1,-1,-1])
+	color=color_white)
 text.draw()
 
 text.color = [-1,0,-1]
