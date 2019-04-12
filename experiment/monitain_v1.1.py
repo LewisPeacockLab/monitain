7b6756746e752a2347a28272bec45dfb153c5654
katlynHedgpeth <katlyn.hedgpeth@gmail.com>
2019-04-12 15:49:48 -0500

    more stuffs

--- experiment/monitain_v1.1.py
+++ experiment/monitain_v1.1.py
@@ -85,6 +85,8 @@ filename = data_path + ".csv"
 if data_path_exists: 
 	sys.exit("Filename " + data_path + "already exists!")
 
+filename = "Dropbox\ \(LewPeaLab\)/BEHAVIOR/monitain/v1/" + filename
+
 ## Set up Slack notificaitons
 SLACK = dict(
 	channel = '#katieshooks', 
