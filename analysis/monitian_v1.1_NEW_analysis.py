9018b8958e01f8f85e419fb16d913596a852fec3
katlynHedgpeth <katlyn.hedgpeth@gmail.com>
2019-07-19 15:29:03 -0500

    create by block type dfs

--- analysis/monitian_v1.1_NEW_analysis.py
+++ analysis/monitian_v1.1_NEW_analysis.py
@@ -49,3 +49,74 @@ for i, fn in enumerate(fnames):
 
     subj_df['version'] = vers
     df_list.append(subj_df)
+
+df_main = pd.concat(df_list,ignore_index=False)
+df_main = df_main.reset_index()
+# Make a copy to replace inaccurate responses with no RT
+df_main_copy = df_main
+# Use for backup
+
+#Record PM accuracy per trial
+for trial in df_main.index: 
+	probeNum = df_main.loc[trial, 'n_probes']
+	if df_main.loc[trial, 'index'] == 0: #something wrong with first trial 
+		pass
+	elif df_main.loc[trial, 'probe{:d}_acc'.format(probeNum-1)] == 1: #correct PM resp
+		df_main.loc[trial, 'acc'] = 1
+	else:
+		print
+		df_main.loc[trial, 'acc'] = 0
+
+#Replace rt with nan if probe resp was incorrect 
+for i in df_main.index: 
+	for probe in range(0,15): 
+		if df_main.loc[i, 'probe{:d}_acc'.format(probe)] == 0: 
+			df_main.at[i, 'rtProbe{:d}'.format(probe)] = np.nan
+
+#Master list of all rt probe titles
+rtProbes = []
+for rt in range(0,14): #get rid of probe14 bc you'll never look at it 
+	rtProbes.append('rtProbe{:d}'.format(rt))
+
+accCols = []
+for acc in range(0,15): #get rid of probe14 bc you'll never look at it 
+	accCols.append('probe{:d}_acc'.format(acc))
+
+
+######### BY BLOCK TYPE DATAFRAMES #########
+
+def createBlockDFs(str1, str2):
+	block_name = df_main[(df_main['block'] == str1) | (df_main['block'] == str2)]
+	block_name_df = pd.concat([ block_name['subj'], block_name['block'], block_name['acc'], block_name[rtProbes].mean(axis=1), block_name[accCols].mean(axis=1)], axis=1)
+	block_name_df.columns = ['subj', 'block', 'pm_acc', 'meanTrial_rt', 'og_acc']
+	return block_name_df
+
+block_maintain_df = createBlockDFs("('maintain1', 2)", "('maintain2', 5)")
+block_monitor_df = createBlockDFs("('monitor1', 3)", "('monitor2', 6)")
+block_mnm_df = createBlockDFs("('mnm1', 4)", "('mnm2', 7)")
+
+
+
+
+
+
+
+######### BY SUBJECT FIGURES #########
+
+#### MAINTAIN
+
+# PM Accuracy
+ax = sea.barplot(x='subj', y= 'pm_acc', hue= 'block', data=maintain_df, palette="Blues", ci = None)
+plt.xlabel('Subject')
+plt.ylabel('PM accuracy')
+sea.despine()
+plt.savefig(FIGURE_PATH + 'maintain_compare_pmacc.png', dpi = 600)
+plt.close()
+
+# OG Accuracy
+ax = sea.barplot(x='subj', y= 'og_acc', hue= 'block', data=maintain_df, palette="Blues", ci = None)
+plt.xlabel('Subject')
+plt.ylabel('OG accuracy')
+sea.despine()
+plt.savefig(FIGURE_PATH + 'maintain_compare_ogacc.png', dpi = 600)
+plt.close()
