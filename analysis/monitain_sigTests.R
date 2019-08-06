3a8ec1d65f597da27aadc5ebbe53651f8016d191
Katlyn Rose Hedgpeth <katiehedgpeth@cpe-172-100-1-153.twcny.res.rr.com>
2019-08-05 20:28:26 -0500

    t tests

--- analysis/monitain_sigTests.R
+++ analysis/monitain_sigTests.R
@@ -78,78 +78,21 @@ summary(glht(pmCost_lme,
              linfct=mcp(blockType="Tukey")),
         test=adjusted(type="bonferroni"))
 
-### PICK UP HERE
-
 ##########################################
-##  MCB rmANOVA across category combos  ##
-##########################################
-
-model_lme <- lme(mcb ~ clpsd_cond, random=~1|subj, data=mcb_df)
-anova(model_lme)
-
-mcb_aov <- aov(model_lme, data=mcb_df)
-
-# t test comparing phovis phosem
-phovis <- subset(mcb_df,clpsd_cond=="pho/vis",mcb,drop=TRUE)
-phosem <- subset(mcb_df,clpsd_cond=="pho/sem",mcb,drop=TRUE)
-semvis <- subset(mcb_df,clpsd_cond=="sem/vis",mcb,drop=TRUE) #kt added
-t.test(phovis,phosem,paired=TRUE,alternative="two.sided") #PHO
-t.test(phovis,semvis,paired=TRUE,alternative="two.sided") #kt added, VIS
-t.test(semvis,phosem,paired=TRUE,alternative="two.sided") #kt added, SEM
-
-# One sample t test to see if diff from 0
-t.test(phovis)
-t.test(phosem)
-t.test(semvis)
-
-##########################################
-## ANOVA for vis, sem, pho by load type ##
-##########################################
-
-vis_lme <- lme(acc ~ load_type, random=~1|subj, data=vis_df)
-anova(vis_lme)
-summary(glht(vis_lme,
-             linfct=mcp(load_type="Tukey")))
-
-sem_lme <- lme(acc ~ load_type, random=~1|subj, data=sem_df)
-anova(sem_lme)
-summary(glht(sem_lme,
-             linfct=mcp(load_type="Tukey")))
-
-pho_lme <- lme(acc ~ load_type, random=~1|subj, data=pho_df)
-anova(pho_lme)
-summary(glht(pho_lme,
-             linfct=mcp(load_type="Tukey")))
-
-
-##########################################
-# t-tests for vis, sem, pho by load type #
+# t-tests by block type #
 ##########################################    
 
-# t tests for vis 
-vis_single <- subset(vis_df,load_type=="single",acc,drop=TRUE)
-vis_same <- subset(vis_df,load_type=="same",acc,drop=TRUE)
-vis_mixed <- subset(vis_df,load_type=="mixed",acc,drop=TRUE)
-t.test(vis_single,vis_same,paired=TRUE,alternative="two.sided") 
-t.test(vis_single,vis_mixed,paired=TRUE,alternative="two.sided") 
-t.test(vis_same,vis_mixed,paired=TRUE,alternative="two.sided") 
+all_baseline <- subset(all_df, blockType=="Baseline", pm_acc, drop=TRUE)
+all_maintain <- subset(all_df, blockType=="Maintain", pm_acc, drop=TRUE)
+all_monitor <- subset(all_df, blockType=="Monitor", pm_acc, drop=TRUE)
+all_mnm <- subset(all_df, blockType=="MnM", pm_acc, drop=TRUE)
 
+#t.test(all_baseline, all_maintain, paired=TRUE, alternative="two.sided")
+#t.test(all_baseline, all_monitor, paired=TRUE, alternative="two.sided")
+#t.test(all_baseline, all_mnm, paired=TRUE, alternative="two.sided")
 
-# t tests for sem
-sem_single <- subset(sem_df,load_type=="single",acc,drop=TRUE)
-sem_same <- subset(sem_df,load_type=="same",acc,drop=TRUE)
-sem_mixed <- subset(sem_df,load_type=="mixed",acc,drop=TRUE)
-t.test(sem_single,sem_same,paired=TRUE,alternative="two.sided") 
-t.test(sem_single,sem_mixed,paired=TRUE,alternative="two.sided") 
-t.test(sem_same,sem_mixed,paired=TRUE,alternative="two.sided") 
+t.test(all_maintain, all_monitor, paired=TRUE, alternative="two.sided")
+t.test(all_maintain, all_mnm, paired=TRUE, alternative="two.sided")
 
-# t tests for vis 
-pho_single <- subset(pho_df,load_type=="single",acc,drop=TRUE)
-pho_same <- subset(pho_df,load_type=="same",acc,drop=TRUE)
-pho_mixed <- subset(pho_df,load_type=="mixed",acc,drop=TRUE)
-t.test(pho_single,pho_same,paired=TRUE,alternative="two.sided") 
-t.test(pho_single,pho_mixed,paired=TRUE,alternative="two.sided") 
-t.test(pho_same,pho_mixed,paired=TRUE,alternative="two.sided") 
-
-
+t.test(all_monitor, all_mnm, paired=TRUE, alternative="two.sided")
 
