816e653723c86047233881f6c227a8bd5dfd667e
Katlyn Rose Hedgpeth <katiehedgpeth@Katlyns-MacBook-Pro.local>
2019-11-14 23:43:01 -0600

    cost v rt linear regressions

--- analysis/monitain_stats.py
+++ analysis/monitain_stats.py
@@ -149,17 +149,20 @@ monitor_trials = all_df_averaged[(all_df_averaged['blockType'] == 'Monitor')] # 
 mnm_trials = all_df_averaged[(all_df_averaged['blockType'] == 'MnM')] # mnm block trials
 
 maintain_results = maintain_trials.groupby(['subj']).mean().reset_index()
-maintain_cost = maintain_results.drop(columns=['pm_acc', 'meanTrial_rt','og_acc'], axis=1)
-maintain_acc = maintain_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt','og_acc'], axis=1) 
+maintain_cost = maintain_results.drop(columns=['pm_acc', 'meanTrial_rt','og_acc', 'pm_probe_rt'], axis=1)
+maintain_acc = maintain_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt','og_acc', 'pm_probe_rt'], axis=1) 
+maintain_pmRT = maintain_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt', 'og_acc', 'pm_acc'])
 # Remove 'subj' column if you need a subj num indication in accuracy df
 
 monitor_results = monitor_trials.groupby(['subj']).mean().reset_index()
-monitor_cost = monitor_results.drop(columns=['pm_acc', 'meanTrial_rt','og_acc'], axis=1)
-monitor_acc = monitor_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt','og_acc'], axis=1) 
+monitor_cost = monitor_results.drop(columns=['pm_acc', 'meanTrial_rt','og_acc', 'pm_probe_rt'], axis=1)
+monitor_acc = monitor_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt','og_acc', 'pm_probe_rt'], axis=1) 
+monitor_pmRT = monitor_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt', 'og_acc', 'pm_acc'])
 
 mnm_results = mnm_trials.groupby(['subj']).mean().reset_index()
-mnm_cost = mnm_results.drop(columns=['pm_acc', 'meanTrial_rt','og_acc'], axis=1)
-mnm_acc = mnm_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt','og_acc'], axis=1) 
+mnm_cost = mnm_results.drop(columns=['pm_acc', 'meanTrial_rt','og_acc', 'pm_probe_rt'], axis=1)
+mnm_acc = mnm_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt','og_acc', 'pm_probe_rt'], axis=1) 
+mnm_pmRT = maintain_results.drop(columns=['subj', 'pm_cost', 'meanTrial_rt', 'og_acc', 'pm_acc'])
 
 
 ### LOOKING FOR OUTLIERS
@@ -172,14 +175,23 @@ def findOutliers(cost, measure):
 	iqr = q3 - q1
 	return cost[~(measure < (q1 - 1.5 * iqr)) | (measure > (q3 + 1.5 * iqr)) ]
 
+## Outliers - PM cost
 maintain_cost = findOutliers(maintain_cost, maintain_cost.pm_cost)
 monitor_cost = findOutliers(monitor_cost, monitor_cost.pm_cost)
 mnm_cost = findOutliers(mnm_cost, mnm_cost.pm_cost)
 
+## Outliers - Accuracy for last probe
 maintain_acc = findOutliers(maintain_acc, maintain_acc.pm_acc)
 monitor_acc = findOutliers(monitor_acc, monitor_acc.pm_acc)
 mnm_acc = findOutliers(mnm_acc, mnm_acc.pm_acc)
 
+## Outliers - RT for last probe
+maintain_pmRT = findOutliers(maintain_pmRT, maintain_pmRT.pm_probe_rt)
+monitor_pmRT = findOutliers(monitor_pmRT, monitor_pmRT.pm_probe_rt)
+mnm_pmRT = findOutliers(mnm_pmRT, mnm_pmRT.pm_probe_rt)
+
+
+## Combine DFs - cost and accuracy
 
 # Maintain cost versus combined performance
 mainCost_combineAcc = pd.concat([maintain_cost, mnm_acc], axis = 1, sort = False)
@@ -198,6 +210,27 @@ combined_cost = maintain_cost.pm_cost + monitor_cost.pm_cost
 # Combined cost (above) versus combined performance
 mplusmCost_combineAcc = pd.concat([combined_cost, mnm_acc], axis=1, sort=False) 
 
+## Combine DFs - cost and RT
+
+# Maintain cost versus combined RT
+mainCost_combineRT = pd.concat([maintain_cost, mnm_pmRT], axis = 1, sort = False)
+# Monitor cost versus combined RT
+monCost_combineRT = pd.concat([monitor_cost, mnm_pmRT], axis = 1, sort = False)
+# Combined cost versus combined RT
+combineCost_combineRT = pd.concat([mnm_cost, mnm_pmRT], axis = 1, sort = False)
+
+# Maintain cost versus maintain RT
+mainCost_mainRT = pd.concat([maintain_cost, maintain_pmRT], axis = 1, sort = False)
+# Monitor cost versus monitor RT
+monCost_monRT = pd.concat([monitor_cost, monitor_pmRT], axis = 1, sort = False)
+
+# Maintain cost + Monitor cost
+combined_cost = maintain_cost.pm_cost + monitor_cost.pm_cost
+# Combined cost (above) versus combined RT
+mplusmCost_combineRT = pd.concat([combined_cost, mnm_pmRT], axis=1, sort=False)
+
+
+
 # Don't look at subjects where PM cost was removed for being an outlier
 def removeOutliers(df): 
 	for index, row in df.iterrows(): 
@@ -205,6 +238,15 @@ def removeOutliers(df):
 			print(index)
 			df = df.drop([index])
 	return df
+
+def removeOutliers_RT(df): 
+	for index, row in df.iterrows(): 
+		if (math.isnan(row.pm_cost) or math.isnan(row.pm_probe_rt)): 
+			print(index)
+			df = df.drop([index])
+	return df
+
+## Remove outliers - cost and accuracy 	
 
 mainCost_combineAcc = removeOutliers(mainCost_combineAcc)
 monCost_combineAcc = removeOutliers(monCost_combineAcc)
@@ -223,8 +265,30 @@ monCost_monAcc = monCost_monAcc.dropna()
 
 mplusmCost_combineAcc = mplusmCost_combineAcc.dropna()
 
-def regPlot(combinedData, x_label, y_label, color): 
-	sea.regplot(x='pm_cost', y = 'pm_acc', data = combinedData, color = color) 
+
+## Remove outliers - cost and RT
+
+mainCost_combineRT = removeOutliers_RT(mainCost_combineRT)
+monCost_combineRT = removeOutliers_RT(monCost_combineRT)
+combineCost_combineRT = removeOutliers_RT(combineCost_combineRT)
+
+mainCost_mainRT = removeOutliers_RT(mainCost_mainRT)
+monCost_monRT = removeOutliers_RT(monCost_monRT)
+
+# Drop NaNs
+mainCost_combineRT = mainCost_combineRT.dropna()
+monCost_combineRT = monCost_combineRT.dropna()
+combineCost_combineRT = combineCost_combineRT.dropna()
+
+mainCost_mainRT = mainCost_mainRT.dropna()
+monCost_monRT = monCost_monRT.dropna()
+
+mplusmCost_combineRT = mplusmCost_combineRT.dropna()
+
+
+
+def regPlot(combinedData, x_label, y_label, color, y_data): 
+	sea.regplot(x='pm_cost', y = y_data, data = combinedData, color = color) 
 	plt.xlabel(x_label)
 	plt.ylabel(y_label)
 
@@ -238,53 +302,102 @@ def residPlot(combinedData, x_label, y_label, color):
 # pdf for viewing
 # eps for editing
 
+
+### Cost v accuracy
 ## How does maintainance cost affect performance when only maintaining?
-regPlot(mainCost_mainAcc, 'Maintain cost (s)', 'Maintain performance', 'b')
+regPlot(mainCost_mainAcc, 'Maintain cost (s)', 'Maintain performance', 'b', 'pm_acc')
 plt.savefig(FIGURE_PATH + 'maintainCost_v_maintainPerf.png', dpi = 600)
 plt.close()
 
 maintain_maintain_lr = pg.linear_regression(mainCost_mainAcc.pm_cost, mainCost_mainAcc.pm_acc) 
 
+### Cost v RT
+regPlot(mainCost_mainRT, 'Maintain cost (s)', 'Maintain PM probe RT', 'b', 'pm_probe_rt')
+plt.savefig(FIGURE_PATH + 'maintainCost_v_maintainRT.png', dpi = 600)
+plt.close()
 
+maintain_maintain_RT_lr = pg.linear_regression(mainCost_mainRT.pm_cost, mainCost_mainRT.pm_probe_rt)
+
+
+### Cost v accuracy
 ## How does monitoring cost affect performance when only monitoring?
-regPlot(monCost_monAcc, 'Monitor cost (s)', 'Monitor performance', 'r')
+regPlot(monCost_monAcc, 'Monitor cost (s)', 'Monitor performance', 'r', 'pm_acc')
 plt.savefig(FIGURE_PATH + 'monitorCost_v_monitorPerf.png', dpi = 600)
 plt.close()
 
 monitor_monitor_lr = pg.linear_regression(monCost_monAcc.pm_cost, monCost_monAcc.pm_acc)
 
+### Cost v RT
+regPlot(monCost_monRT, 'Monitor cost (s)', 'Monitor PM probe RT', 'r', 'pm_probe_rt')
+plt.savefig(FIGURE_PATH + 'monitorCost_v_monitorRT.png', dpi = 600)
+plt.close()
+
+monitor_monitor_RT_lr = pg.linear_regression(monCost_monRT.pm_cost, monCost_monRT.pm_probe_rt)
 
 
+
+### Cost v accuracy
 ## How does maintaining cost affect PM performance?
-regPlot(mainCost_combineAcc, 'Maintain cost (s)','Combined performance', 'b')
+regPlot(mainCost_combineAcc, 'Maintain cost (s)','Combined performance', 'b', 'pm_acc')
 plt.savefig(FIGURE_PATH + 'maintainCost_v_pmAcc.png', dpi = 600)
 plt.close()
 
 maintain_combine_lr = pg.linear_regression(mainCost_combineAcc.pm_cost, mainCost_combineAcc.pm_acc)
 
+### Cost v RT
+regPlot(mainCost_combineRT, 'Maintain cost (s)','Combined PM probe RT', 'b', 'pm_probe_rt')
+plt.savefig(FIGURE_PATH + 'maintainCost_v_pmRT.png', dpi = 600)
+plt.close()
 
+maintain_combine_RT_lr = pg.linear_regression(mainCost_combineRT.pm_cost, mainCost_combineRT.pm_probe_rt)
+
+
+### Cost v accuracy
 ## How does monitoring cost affect PM performance?
-regPlot(monCost_combineAcc, 'Monitor cost (s)','Combined performance', 'r')
+regPlot(monCost_combineAcc, 'Monitor cost (s)','Combined performance', 'r', 'pm_acc')
 plt.savefig(FIGURE_PATH + 'monitorCost_v_pmAcc.png', dpi = 600)
 plt.close()
 
 monitor_combine_lr = pg.linear_regression(monCost_combineAcc.pm_cost, monCost_combineAcc.pm_acc)
 
+### Cost v RT
+regPlot(monCost_combineRT, 'Monitor cost (s)','Combined PM probe RT', 'r', 'pm_probe_rt')
+plt.savefig(FIGURE_PATH + 'monitorCost_v_pmRT.png', dpi = 600)
+plt.close()
 
+monitor_combine_RT_lr = pg.linear_regression(monCost_combineRT.pm_cost, monCost_combineRT.pm_probe_rt)
+
+
+### Cost v accuracy
 ## How does PM cost affect performance when maintaining AND monitoring?
-regPlot(combineCost_combineAcc, 'MnM cost (s)','Combined performance', 'purple')
+regPlot(combineCost_combineAcc, 'MnM cost (s)','Combined performance', 'purple', 'pm_acc')
 plt.savefig(FIGURE_PATH + 'mnmCost_v_pmAcc.png', dpi = 600)
 plt.close()
 
 combine_combine_lr = pg.linear_regression(combineCost_combineAcc.pm_cost, combineCost_combineAcc.pm_acc)
 
+### Cost v RT
+regPlot(combineCost_combineRT, 'MnM cost (s)','Combined PM probe RT', 'purple', 'pm_probe_rt')
+plt.savefig(FIGURE_PATH + 'mnmCost_v_pmRT.png', dpi = 600)
+plt.close()
 
+combine_combine_RT_lr = pg.linear_regression(combineCost_combineRT.pm_cost, combineCost_combineRT.pm_probe_rt)
+
+
+### Cost v accuracy
 ## How does additive maintainence and monitoring cost affect performance when maintaining AND monitoring?
-regPlot(mplusmCost_combineAcc, 'Maintainence + Monitoring cost (s)','Combined performance', 'purple')
+regPlot(mplusmCost_combineAcc, 'Maintainence + Monitoring cost (s)','Combined performance', 'purple', 'pm_acc')
 plt.savefig(FIGURE_PATH + 'mplusmCost_v_pmAcc.png', dpi = 600)
 plt.close()
 
 mplusm_combineAcc_lr = pg.linear_regression(mplusmCost_combineAcc.pm_cost, mplusmCost_combineAcc.pm_acc)
+
+### Cost v RT
+regPlot(mplusmCost_combineRT, 'Maintainence + Monitoring cost (s)','Combined PM probe RT', 'purple', 'pm_probe_rt')
+plt.savefig(FIGURE_PATH + 'mplusmCost_v_pmRT.png', dpi = 600)
+plt.close()
+
+mplusm_combine_RT_lr = pg.linear_regression(mplusmCost_combineRT.pm_cost, mplusmCost_combineRT.pm_probe_rt)
 
 
 
