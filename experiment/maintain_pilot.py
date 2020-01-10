800d9c6706b0dd4308de0b5f4fc274de447653d4
katlynHedgpeth <katlyn.hedgpeth@gmail.com>
2020-01-10 12:15:00 -0600

    read csv and import stims

--- experiment/maintain_pilot.py
+++ experiment/maintain_pilot.py
@@ -9,6 +9,7 @@ January 2020
 
 # import all needed modules
 import argparse
+import csv
 import glob
 import itertools
 import os
@@ -18,7 +19,7 @@ import requests
 import sys
 
 import numpy as np
-import pandas as pandas
+import pandas as pd
 
 from collections import OrderedDict
 from itertools import product, compress
@@ -34,7 +35,7 @@ args = parser.parse_args()
 
 SUBJ = args.subj
 
-### set up Slack ###
+### Set up Slack ###
 
 # set to false because github respository is public
 SLACK = False
@@ -53,11 +54,91 @@ def slack(msg):
 	print msg
 
 
-### set up directories ## 
+### Directories and import data ## 
 
 DATA_PATH = 'monitain_v1.1_MAINTAIN_' + str(SUBJ)
 DATA_FNAME = DATA_PATH + '.csv'
 if os.path.exists(DATA_FNAME):
 	sys.exit('Filename ' + DATA_FNAME + " already exists!")
+
+# import images
+images = glob.glob('stimuli/grayscale/*.png')
+
+word_file = 'words.csv'
+nonword_file = 'nonwords.csv'
+pract_word_file = 'pract_words.csv'
+pract_nonword_file = 'pract_nonwords.csv'
+
+def read_csv(filename):
+	export_list = []
+	# open and read csv
+	with open(filename, 'r') as csvfile: 
+		csvreader = csv.reader(csvfile)
+		for row in csvreader: 
+			export_list.append(row)
+	# shuffle stimuli
+	random.shuffle(export_list) 
+	# return shuffled list 
+	return export_list
+
+# import words and nonwords
+word_list = read_csv(word_file)
+nonword_list = read_csv(nonword_file)
+pract_word_list = read_csv(pract_word_file)
+pract_nonword_list = read_csv(pract_nonword_file)
+
+
+
+
+### General experiment parameters ###
+
+N_MIN_PROBES = 8
+N_MAX_PROBES = 15
+N_PROBES_PER_BLOCK = 20
+
+LOWER_CATCH_TRIAL = 1
+UPPER_CATCH_TRIAL = 7
+
+N_BLOCKS = 4
+
+# if you want to change number of blocks of each type, change here
+N_BLOCKS_MAINTAIN_1 = 1
+N_BLOCKS_MAINTAIN_1 = 1
+N_BLOCKS_MAINTAIN_1 = 1
+
+N_TRIALS = range(N_MIN_PROBES, N_MAX_PROBES+1)
+
+BASELINE_TRIALS = 106
+MAINTAIN_TRIALS = 20
+
+N_TOTAL_TRIALS = (BASELINE_TRIALS*2) + (MAINTAIN_TRIALS*3)
+N_TOTAL_TRIALS_PRACT = 5 * N_BLOCKS # 5 for each baseline, maintain1, maintain2, maintain3
+
+# colors
+color_white = [255,255, 255] #[1,1,1]
+color_black = [0,0,0] #[-1,-1,-1]
+color_gray = [85,85,85] #[0,0,0]
+color_cyan = [0,255,255] #[0,1,1]
+color_green = [0,255,0] #[0,1,0]
+color_red = [255,0,0] #[1,0,0]
+color_blue = [0,0,255]
+
+# event timings
+TIMINGS = {
+	'target': 		2,
+	'delay': 		1, 
+	'probe': 		2, 
+	'iti': 			1,
+}
+
+BLOCK_ORDER = {
+	'base1': 		1, 
+	'maintain1': 	2, 
+	'maintain2':	3, 
+	'maintain3':	4,
+}
+
+
+### Master dataframe
 
 
